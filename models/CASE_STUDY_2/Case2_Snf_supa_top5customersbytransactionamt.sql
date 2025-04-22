8b11fb19a41c700bb8c9e940e410e6308805c7f7
Fathima Syed <syed9949946736@gmail.com>
2025-04-22 10:57:40 +0000

    data

---
+++ models/CASE_STUDY_2/Case2_Snf_supa_top5customersbytransactionamt.sql
@@ -0,0 +1,15 @@
+select * from (select 
+    T."Transaction_ID" ,
+	T."Name",
+	T."Transaction_Date" ,
+	T."Transaction_Type" ,
+	T."Transaction_Amount",
+	T."Transaction_Mode" ,
+	B.ACCOUNTNUMBER ,
+	B.BRANCH,
+   dense_rank() over (order by T."Transaction_Amount"  desc ) as  rnk  
+from 
+MYDB.DBT.XML_SNF_CASESTD2_TRANSACTION_DETAILS  as T
+INNER JOIN 
+MYDB.DBT.JSON_SNF_CASESTD2_BANKING_DATA as B
+ON T."Name" =B.CUSTOMER_ID)  where  rnk <=5
---
+++ models/CASE_STUDY_2/Case_study_2_Customer_transactions.sql
@@ -0,0 +1,14 @@
+select 
+    T."Transaction_ID" ,
+	T."Name",
+	T."Transaction_Date" ,
+	T."Transaction_Type" ,
+	T."Transaction_Amount",
+	T."Transaction_Mode" ,
+	B.ACCOUNTNUMBER ,
+	B.BRANCH 
+from 
+MYDB.DBT.XML_SNF_CASESTD2_TRANSACTION_DETAILS  as T
+INNER JOIN 
+MYDB.DBT.JSON_SNF_CASESTD2_BANKING_DATA as B
+ON T."Name" =B.CUSTOMER_ID
